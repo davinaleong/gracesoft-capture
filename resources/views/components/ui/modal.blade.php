1b39bdcc3e9fb51309159e6c39ebf0ba43d06b68
Davina Leong <leong.shi.yun@gmail.com>
2026-04-01 14:24:04 +0800

    Centered the modal.

--- resources/views/components/ui/modal.blade.php
+++ resources/views/components/ui/modal.blade.php
@@ -5,10 +5,14 @@
 ])
 
 @php
-    $containerClass = trim('fixed left-1/2 top-1/2 h-fit max-h-[calc(100vh-2rem)] w-[calc(100%-1rem)] -translate-x-1/2 -translate-y-1/2 overflow-y-auto rounded border border-gray-200 p-0 backdrop:bg-black/40 ' . $maxWidth);
+    $containerClass = trim('h-fit w-[calc(100%-1rem)] overflow-y-auto rounded border border-gray-200 p-0 backdrop:bg-black/40 ' . $maxWidth);
 @endphp
 
-<dialog @if ($id) id="{{ $id }}" @endif {{ $attributes->class([$containerClass]) }}>
+<dialog
+    @if ($id) id="{{ $id }}" @endif
+    style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); max-height: calc(100vh - 2rem);"
+    {{ $attributes->class([$containerClass]) }}
+>
     <x-ui.card class="space-y-4 border-0 shadow-none">
         @if ($title)
             <h2 class="text-xl font-bold">{{ $title }}</h2>
